5f258f5a612d52f32a6418e367be6df44fe8a68d
FuzzyJones <donald.jones83@gmail.com>
2017-12-26 11:46:28 -0500

    worked on js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,14 @@ var losses = 0;
 var guessesLeft = 9;
 var lettersGuessed = [];
 
+var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
 
 
 // game function
 document.onkeyup = function(event) {
     
     // user guess
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userGuess = (event.key).toLowerCase();
     // computer random word
     var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];
     // pushing the user selected letter to the letters guessed
@@ -21,11 +22,32 @@ document.onkeyup = function(event) {
 
     // if else statements
     if (userGuess === computerGuess) {
-        win++;
+        wins++;
+        guessesLeft = 9;
+        lettersGuessed = [];
+        var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];        
         }  
-    else if (userGuess !== computerGuess){
-        guessesLeft--; 
-    }   
+    if (userGuess !== computerGuess){
+        guessesLeft--;   
+    }
+    if (guessesLeft === 0) {
+        losses++;
+        guessesLeft = 9;
+        lettersGuessed = [];
+        var computerGuess = letterOptions[Math.floor(Math.random() * letterOptions.length)];        
+    }
+    
+    var html = 
+        "<h1>The Psychic Game</h1>" +
+        "<p>Guess what letter I'm thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Loses: " + losses + "</p>" +
+        "<p>Guesses Left: " + guessesLeft + "</p>" +
+        "<p>Guesses so far: " + lettersGuessed + "</p>";
 
+    document.querySelector("#game").innerHTML = html;
+
+    // console.log(userGuess);
+    // console.log(computerGuess);
 
 };
